136bcc316dc8cf8447f5b436be8513be0ddc2e96
Kat-Erina <nalchevanidzekaterina@gmail.com>
2023-11-08 21:18:08 +0400

    initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+// Project#1
+
+// let name = prompt("What is your name?");
+// console.log(`Hello, ${name}`);
+
+//#2
+// let inp = prompt("Please write anything you like");
+// console.log(`Your provided string consists of ${inp.length} symbols`);
+
+//#3
+
+// let str = prompt("What is the quote?");
+// let author = prompt("Who said it");
+
+// console.log(
+//   `What is the quote? ${str},\nWho said that? ${author}.\n${author} says, '${str}'`
+// );
+
+//4
+
+let val = prompt("Please write anything");
+console.log(val.toLowerCase());
+//5
+
